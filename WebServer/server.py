8c5f6db180ea86062aa9afd6dec1f20495758523
kaptainkohl <blake.kohl@me.com>
2017-08-01 15:48:25 -0500

    changes

--- WebServer/server.py
+++ WebServer/server.py
@@ -22,32 +22,8 @@ f = open('ip.txt', 'r')
 HOST = f.readline()
 f.close()
 print(HOST)
-PORT = 8888 
 quit= True
 	
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print ("Socket created")
- 
-#Bind socket to local host and port
-try:
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	s.bind((HOST, PORT))
-except socket.error as msg:
-	print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-	sys.exit()
-	 
-print ('Socket bind complete')
-s.listen(15)
-print ('Socket now listening')
-	
-
-def socketThread():
-	print("now connect")
-	while quit:
-		conn, addr = s.accept()
-		print ('Connected with ' + addr[0] + ':' + str(addr[1]))
-		start_new_thread(clientthread ,(conn,))
-	s.close()
 
 if __name__ == '__main__':
 	#start_new_thread(write ,())
@@ -55,32 +31,6 @@ if __name__ == '__main__':
 	
 	app.run()
 	
-
-
-
- 
-#Function for handling connections
-def clientthread(conn):
-	global index
-	global user_data
-	global quit
-	conn.send('connected')
-	datam=conn.recv(1024)
-	#print(datam)
-	
-	while quit:
-		try:
-			data=conn.recv(1024).split(',')
-			#print(data[0])
-			for x in range(0,len(user_name)):
-				if user_name[x] == data[0]:
-					user_data[x][0]= str(data[1])
-					user_data[x][1]= str(data[2])
-					user_data[x][2]= str(data[3])
-		except:
-			print("quiting")
-			quit = False
-	conn.close()
 
 def write():
 	global user_data
@@ -90,7 +40,6 @@ def write():
 	for x in range(0,len(user_name)):
 		totals = ''+totals + user_name[x] +","+str(user_data[x])[1:-1]+"$"
 
-	
 	
 
 @app.route('/_update')
