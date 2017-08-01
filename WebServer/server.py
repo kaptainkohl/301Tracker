84f4dc5c328a63cdb85ef3ef160d98b886e6279a
Blake Kohl <blake.kohl@me.com>
2017-08-01 08:22:14 -0500

    Update server.py

--- WebServer/server.py
+++ WebServer/server.py
@@ -18,69 +18,16 @@ totals=''
 y =0
 a =0
 
-f = open('ip.txt', 'r')
-HOST = f.readline()
-f.close()
-print(HOST)
-PORT = 8888 
+
 quit= True
 	
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print ("Socket created")
- 
-#Bind socket to local host and port
-try:
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	s.bind((HOST, PORT))
-except socket.error as msg:
-	print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-	sys.exit()
-	 
-print ('Socket bind complete')
-s.listen(15)
-print ('Socket now listening')
-	
-
-def socketThread():
-	print("now connect")
-	while quit:
-		conn, addr = s.accept()
-		print ('Connected with ' + addr[0] + ':' + str(addr[1]))
-		start_new_thread(clientthread ,(conn,))
-	s.close()
 
 if __name__ == '__main__':
 	#start_new_thread(write ,())
 	#start_new_thread(socketThread ,())
-	
-	app.run()
-	
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
 
-
-
- 
-#Function for handling connections
-def clientthread(conn):
-	global index
-	global user_data
-	global quit
-	conn.send('connected')
-	datam=conn.recv(1024)
-	#print(datam)
-	
-	while quit:
-		try:
-			data=conn.recv(1024).split(',')
-			#print(data[0])
-			for x in range(0,len(user_name)):
-				if user_name[x] == data[0]:
-					user_data[x][0]= str(data[1])
-					user_data[x][1]= str(data[2])
-					user_data[x][2]= str(data[3])
-		except:
-			print("quiting")
-			quit = False
-	conn.close()
 
 def write():
 	global user_data
